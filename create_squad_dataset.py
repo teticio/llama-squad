5afe5ca072ee97b698a3ec49ec8b0297bfeff35c
Robert Dargavel Smith <teticio@gmail.com>
2023-07-29 16:40:19 +0100

    add base dataset

--- create_squad_dataset.py
+++ create_squad_dataset.py
@@ -1,24 +1,66 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
 from datasets import Dataset, DatasetDict, load_dataset
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, HfArgumentParser
 
 from utils import DEFAULT_SYSTEM_PROMPT, get_prompt
+
+
+@dataclass
+class ScriptArguments:
+    base: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Use base instruction format."},
+    )
+
 
 SYSTEM_PROMPT = DEFAULT_SYSTEM_PROMPT
 REASONING_LENGTH = 100
 reasoning = " ".join(["blah"] * REASONING_LENGTH)
 
 
+def get_base_prompt_and_response(item):
+    context = item["context"]
+    question = item["question"]
+    answer = item["answers"]["text"][0] if len(item["answers"]["text"]) > 0 else "?"
+    return {
+        "text": get_prompt(
+            f"""\
+Use the following context to answer the question and give the answer in JSON format as follows:
+```json
+{{
+  "answer": ...
+}}
+```
+Context: {context}
+Question: {question}""",
+            [],
+            SYSTEM_PROMPT,
+        )
+        + f""" \
+```json
+{{
+  "answer": "{answer}"
+}}
+``` </s>"""
+    }
+
+
 def get_prompt_and_response(item):
     context = item["context"]
     question = item["question"]
     answer = item["answers"]["text"][0] if len(item["answers"]["text"]) > 0 else "?"
-    return (
-        get_prompt(
+    return {
+        "text": get_prompt(
             """\
 Now give the answer in JSON format as follows:
 ```json
 {
   "answer": ...
-}""",
+}
+```
+""",
             [
                 (
                     f"""\
@@ -42,18 +84,20 @@ Extract the minimal span word for word from the context that best answers the qu
 ```json
 {{
   "answer": "{answer}"
-}} </s>"""
-    )
+}}
+```
+ </s>"""
+    }
 
+
+parser = HfArgumentParser(ScriptArguments)
+script_args = parser.parse_args_into_dataclasses()[0]
 
 squad_dataset = load_dataset("squad_v2")
-train_data = {
-    "text": [get_prompt_and_response(item) for item in squad_dataset["train"]]
-}
-test_data = {
-    "text": [get_prompt_and_response(item) for item in squad_dataset["validation"]]
-}
-dataset = DatasetDict(
-    {"train": Dataset.from_dict(train_data), "test": Dataset.from_dict(test_data)}
+instruction = (
+    get_base_prompt_and_response if script_args.base else get_prompt_and_response
 )
+train_dataset = squad_dataset["train"].map(instruction)
+test_dataset = squad_dataset["validation"].map(instruction)
+dataset = DatasetDict({"train": train_dataset, "test": test_dataset})
 dataset.save_to_disk("data/squad_v2")
